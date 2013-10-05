ece335739d289b4a98fb83e79b42708a624c3605
Dimpurr <dimpurr@live.com>
2013-10-05 18:19:16 +0800

    V1.55 尾部、评论细节更正
    * 修正无自定义统计信息时显示
    * 减小评论间距
    * 调试更新提醒 Bug

--- footer.php
+++ footer.php
@@ -2,7 +2,19 @@
 
 Design by <a href="http://dimpurr.com" title="Dimpurr (钉子)">Dimpurr</a><br />
 Theme <a href="http://blog.dimpurr.com/clearision" title="Clearision">Clearision</a><br />
-<a href="http://wordpress.org/" title="WordPress">Wordress</a> <?php echo get_option('clrs_tongji'); ?><br /><?php wp_footer(); ?>
+
+<?php
+
+$clrs_tjt = get_option('clrs_tongji');
+
+if ( !empty($clrs_tjt) ) { ?>
+	<a href="http://wordpress.org/" title="WordPress">Wordress</a>
+	<?php echo $clrs_tjt;
+} else { ?>
+	Power by <a href="http://wordpress.org/" title="WordPress">Wordress</a>
+<?php }; ?>
+
+<?php wp_footer(); ?>
 
 </footer>
 
