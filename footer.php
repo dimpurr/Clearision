fc3790835040b8f0b331d0f61dc75468ee753fdc
Dimpurr <dimpurr@live.com>
2013-11-28 15:34:13 +0800

    V2.0 修复和发布准备
    * 尾部 Powered by 修复
    * 部分样式修复
    * 调整和发布准备

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <footer id="footer" role="contentinfo">
 <?php $clrs_tjt = get_option('clrs_tongji'); if ( !empty($clrs_tjt) ) { echo $clrs_tjt;}; ?>
-Designed by <a href="http://dimpurr.com" title="Dimpurr (钉子)">Dimpurr</a><br />
-Theme as <a href="http://blog.dimpurr.com/clearision" title="Clearision">Clearision</a><br />
+<a href="http://dimpurr.com" title="Dimpurr (钉子)">Dimpurr</a>'s 
+<a href="http://blog.dimpurr.com/clearision" title="Clearision">Clearision</a><br />
 Powered by <a href="http://wordpress.org/" title="WordPress">WordPress</a>
 
 <script src="<?php echo get_template_directory_uri(); ?>/js/script.js" type="text/javascript"></script>
