870d5caa29750aee603f34d5685356c4f47adcde
fengberd <admin@berd.moe>
2020-03-24 21:10:43 +0800

    JS 重构, 移除未使用文件, 更新 HTML5 Shiv 3.7.3

--- header.php
+++ header.php
@@ -33,7 +33,7 @@
 	<link rel="alternate" type="application/atom+xml" title="ATOM 1.0" href="<?php bloginfo('atom_url') ?>" />
 
 	<!--[if lt IE 9]>
-	<script src="<?= get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
+	<script src="<?= get_template_directory_uri(); ?>/js/html5shiv.min.js" type="text/javascript"></script>
 	<![endif]-->
 
 	<?php
